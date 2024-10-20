6398d83f66e003cb9c34bbc7ef296bc5f4ab01d8
Brynn Lewin <bhl21@fsu.edu>
2024-10-20 13:17:41 -0400

    done!

--- sketch.js
+++ sketch.js
@@ -1,39 +1,17 @@
 let dice = [];
-let numberOfDice = 9;
+let numberOfDice = 6;
 
-
-let font;
-let fontSize = 40;
-
-
-function preload(){
- font = loadFont("assets/Barber Shop.otf");
-}
 
 
 function setup() {
  createCanvas(windowWidth, windowHeight);
  for (let i = 0; i < numberOfDice; i++) {
-   dice[i] = new Die(50); // argument is the size of the die
+   dice[i] = new Die(100); // argument is the size of the die
  }
 
-
- textFont(font);
- textSize(fontSize);
-
- textAlign(LEFT, CENTER);
- drawWords(width * height);
-
 }
 
 
-function drawWords (x) {
- fill(80);
-
- text("words", 100, 80);
- text("hey",200, 150);
- text("oh", 300, 290);
-}
 
 
 function draw() {
@@ -41,7 +19,8 @@ function draw() {
   // loop over the array and place+display each die
 
   fill(255);
-  text("words on screen", 400, 400);
+  textSize(20);
+  text("TO COMPLETE THIS GAME, ROLL AND CLICK THE DICE UNTILL THE NUMBER 1-6 ARE IN ORDER. GOOD LUCK!", 600, 400);
 
  for (let i = 0; i < dice.length; i++) {
    const die = dice[i]; // 'die' is a temporary variable for the current array item
